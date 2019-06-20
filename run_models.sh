8eb7b1067e9928befcf7d672b3480713c4ea8da2
Josiah Schwab <jschwab@gmail.com>
2019-06-20 10:31:44 -0700

    Switch scheduler to SLURM

--- run_models.sh
+++ run_models.sh
@@ -1,20 +1,26 @@
 #!/bin/bash -x
-#PBS -N AIC
-#PBS -l nodes=1:ppn=16
-#PBS -l walltime=24:00:00
-#PBS -V
-#PBS -m abe
-#PBS -M jwschwab@ucsc.edu
+#SBATCH --job-name=AIC
+#SBATCH --partition=shared
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --mem=40G
+#SBATCH --export=ALL
+#SBATCH --time=08:00:00
+#SBATCH --array 1-6
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jwschwab@ucsc.edu
+#SBATCH -A csc116
 
-if [ -n "${PBS_O_WORKDIR}" ]; then
-   cd ${PBS_O_WORKDIR}
-   module load mesasdk/20180127
-   export MESA_DIR=/pfs/jschwab/mesa-r10398
-   export OMP_NUM_THREADS=16
+if [ -n "${SLURM_SUBMIT_DIR}" ]; then
+   cd ${SLURM_SUBMIT_DIR}
+   module load mesasdk/20190503
+   export MESA_DIR=${PROJECT}/mesa-r10398
+   export MESA_CACHES_DIR=/oasis/scratch/comet/jschwab/temp_project/mesa-r10398/cache
+   export OMP_NUM_THREADS=8
 else
     # assume you're already there
-    PBS_O_WORKDIR=$(pwd)
-    PBS_ARRAYID=1
+    SLURM_SUBMIT_DIR=$(pwd)
+    SLURM_ARRAY_TASK_ID=1
 fi
 
 # rebuild MESA
@@ -32,42 +38,42 @@ do_one() {
     read ID INLIST_IO INLIST_VARIABLE TRANSITIONS_FILE <<< $(sed "${1}q;d" < $2)
 
     # use the main inlist in the submit directory
-    export MESA_INLIST=${PBS_O_WORKDIR}/inlist
+    export MESA_INLIST=${SLURM_SUBMIT_DIR}/inlist
 
     # make a temporary directory
     TMPDIR=$(mktemp -d)
     cd ${TMPDIR}
 
-    # cache locally
-    mkdir -p caches
-    export MESA_CACHES_DIR=$(pwd)/caches
-
     # setup inlists via soft links
-    ln -sf ${PBS_O_WORKDIR}/inlist_fixed .
-    ln -sf ${PBS_O_WORKDIR}/inlists/${INLIST_VARIABLE} inlist_variable
-    ln -sf ${PBS_O_WORKDIR}/inlists/${INLIST_IO} inlist_io
-    ln -sf ${PBS_O_WORKDIR}/inlist_pgstar .
+    ln -sf ${SLURM_SUBMIT_DIR}/inlist_fixed .
+    ln -sf ${SLURM_SUBMIT_DIR}/inlists/${INLIST_VARIABLE} inlist_variable
+    ln -sf ${SLURM_SUBMIT_DIR}/inlists/${INLIST_IO} inlist_io
+    ln -sf ${SLURM_SUBMIT_DIR}/inlist_pgstar .
     
     # softlink in some more stuff
-    ln -sf ${PBS_O_WORKDIR}/history_columns.list .
-    ln -sf ${PBS_O_WORKDIR}/profile_columns.list .
-    ln -sf ${PBS_O_WORKDIR}/models .
-    mkdir -p ${PBS_O_WORKDIR}/output/${ID}
-    ln -sf ${PBS_O_WORKDIR}/output/${ID} LOGS
+    ln -sf ${SLURM_SUBMIT_DIR}/history_columns.list .
+    ln -sf ${SLURM_SUBMIT_DIR}/profile_columns.list .
+    ln -sf ${SLURM_SUBMIT_DIR}/models .
+    mkdir -p ${SLURM_SUBMIT_DIR}/output/${ID}
+    ln -sf ${SLURM_SUBMIT_DIR}/output/${ID} LOGS
 
     # set up states and transitions
-    ln -sf ${PBS_O_WORKDIR}/urca.net .
-    ln -sf ${PBS_O_WORKDIR}/weak.net .
-    ln -sf ${PBS_O_WORKDIR}/weak.states .
-    ln -sf ${PBS_O_WORKDIR}/transitions/${TRANSITIONS_FILE} weak.transitions
+    ln -sf ${SLURM_SUBMIT_DIR}/urca.net .
+    ln -sf ${SLURM_SUBMIT_DIR}/weak.net .
+    ln -sf ${SLURM_SUBMIT_DIR}/weak.states .
+    ln -sf ${SLURM_SUBMIT_DIR}/transitions/${TRANSITIONS_FILE} weak.transitions
     
     # run MESA
-    ${PBS_O_WORKDIR}/star
+    ${SLURM_SUBMIT_DIR}/star
 
     # make movie
     DATE=$(date +%F)
-    images_to_movie.sh 'png/grid1*.png' ${PBS_O_WORKDIR}/movies/${ID}-${DATE}.mp4
+    images_to_movie 'png/grid1*.png' ${SLURM_SUBMIT_DIR}/movies/${ID}-${DATE}.mp4
 
+    cd -
+    rm -rf ${TMPDIR}
+
+    
 }
 
-do_one ${PBS_ARRAYID} AIC.batch
+do_one ${SLURM_ARRAY_TASK_ID} AIC.batch
